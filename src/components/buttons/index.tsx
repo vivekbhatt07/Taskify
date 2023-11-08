afbf051a82a4e30c9b9516585459b002091edb15
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-11-08 12:17:18 +0530

    add: button component

---
+++ src/components/buttons/index.tsx
@@ -0,0 +1,5 @@
+import TextButton from "./TextAction";
+import IconButton from "./IconAction";
+import TolltipIconAction from "./TooltipIconAction";
+
+export { TextButton, IconButton, TolltipIconAction };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
+import { IconButton, TextButton, TolltipIconAction } from "../../components";
+import AddIcon from "@mui/icons-material/Add";
 
 interface DashboardProps {}
 
 const Dashboard: FC<DashboardProps> = () => {
-  return <div className="text-xl">Dashboard</div>;
+  return (
+    <div className="text-xl m-6">
+      <TextButton>Click</TextButton>
+      <TolltipIconAction>
+        <AddIcon />
+      </TolltipIconAction>
+    </div>
+  );
 };
 
 export default Dashboard;
