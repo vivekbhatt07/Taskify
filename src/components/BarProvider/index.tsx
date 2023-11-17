cd8cea7b2bc429eb8175302632d6aa6b6881a642
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-11-17 23:06:23 +0530

    add: bar graph

---
+++ src/components/BarProvider/index.tsx
@@ -0,0 +1,25 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function BarProvider({ options, data }) {
+  return <Bar options={options} data={data} />;
+}
+
+export default BarProvider;
